4821af60550653d36b823263094ebaed9c90bf78
Padmashree14santhosh <92292526+Padmashree14santhosh@users.noreply.github.com>
2021-11-20 19:30:18 +0530

    Add files via upload

---
+++ task_3.js
@@ -0,0 +1,37 @@
+// Create your own resume data in JSON format
+let Resume=[{
+    Name:"Padmashree N",
+    Phone_number:"7975564305",
+    Higher_Education:{
+        education:"M.tech(CSE)",
+        Percentage:"75%",
+        college:"SIT",
+        location:"Tumkur",
+    },
+    Degree:{
+        education:"B.E(CSE)",
+        Percentage:"77%",
+        college:"SSIT",
+        location:"Tumkur",
+    },
+    Secondary_school:{
+        education:"PUC(PCMB)",
+        Percentage:"83%",
+        college:"BGS",
+        location:"Magadi",
+    },
+    school:{
+        education:"10th",
+        Percentage:"80%",
+        School:"Magadi Kempegowda School",
+        location:"Magadi",
+    },
+    Skills:['javascript','Python','Machine Learning','HTML'],
+    Known_languages:{
+        language1:"Kannada",
+        language2:"English"
+    }
+},
+];
+
+console.table(Resume);
